eca5b9a906782fd7f22e336deb595f7ede30a499
Lasha <lasha.liluashvili@makingscience.com>
2024-10-03 15:56:39 +0400

    firt push

--- database/migrations/2024_10_03_104829_add_indexes_to_tasks_table.php
+++ database/migrations/2024_10_03_104829_add_indexes_to_tasks_table.php
@@ -3,26 +3,30 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
             $table->index('title');
-            $table->index('description');
             $table->index('task_status');
             $table->index('due_date');
         });
+
+        DB::statement('CREATE INDEX tasks_description_index ON tasks (description(255))');
+
     }
 
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
             $table->dropIndex(['title']);
-            $table->dropIndex(['description']);
             $table->dropIndex(['task_status']);
             $table->dropIndex(['due_date']);
         });
+
+        DB::statement('DROP INDEX tasks_description_index ON tasks');
+
     }
 };
